1e967404c9eebb4d9c6828f6864b19be73f90570
Alexis Baud <alexis@Macbook-Pro-de-Alexis.local>
2024-03-12 16:55:42 +0100

    salut ça va ?

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import Reac, { Component } from "react";
+
+class Test extends Component {
+  render() {
+    return (
+      <div className="Test">
+        <h1>Salut</h1>
+        <p>Ge souis in potit paragrafe tou migniont</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
